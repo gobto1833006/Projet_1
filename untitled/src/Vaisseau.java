2582d8e4642dc0cb21f774d7005cc844e80056ac
gobto1833006 <gobto1833006@etu.cegepjonquiere.ca>
2019-08-26 17:35:17 -0400

    Boy ca va être long

---
+++ untitled/src/Vaisseau.java
@@ -0,0 +1,12 @@
+import Item.Items;
+
+import java.util.ArrayList;
+
+public class Vaisseau {
+
+    private int carburant;
+    private int pointDeVue;
+    private ArrayList<Items> inventaire;
+
+
+}
